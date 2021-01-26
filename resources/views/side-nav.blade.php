3c9c81f478051433890a9d17a514f39156b2577d
juan <juanignaciocasais@gmail.com>
2021-01-25 23:11:44 -0300

    side-nav y extras

---
+++ resources/views/side-nav.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('side-nav')
+
+<div class="float-left list-group">
+  <li class="list-group-item list-group-item-action active">Categories</li>
+  <a href="#" class="list-group-item list-group-item-action">1</a>
+  <a href="#" class="list-group-item list-group-item-action">2</a>
+  <a href="#" class="list-group-item list-group-item-action">3</a>
+  <a href="#" class="list-group-item list-group-item-action">4</a>
+</div>
+
+@endsection
