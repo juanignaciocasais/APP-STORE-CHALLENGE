ce84c81a93ea1f0707a8d23a4497054f45128890
juan <juanignaciocasais@gmail.com>
2021-01-29 00:50:30 -0300

    Implemento seeders para las categorias y dos usuarios con roles diferentes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert([
+            'name' => 'developer',
+            'email' => 'developer@mail.com',
+            'password' =>  Hash::make('1234qwer'),
+            'user_type' => 'Developer',
+        ]);
+        DB::table('users')->insert([
+            'name' => 'client',
+            'email' => 'client@mail.com',
+            'password' => Hash::make('1234qwer'),
+            'user_type' => 'Client',
+        ]);
+
         DB::statement("INSERT INTO `categories` (`category_id`, `category_name`) VALUES
             (1, 'Games'),
             (2, 'Sports'),
