9d93b5f55f3db129de3c0b14e31bb3a00f6cc909
juan <juanignaciocasais@gmail.com>
2021-01-30 20:45:33 -0300

    habilitar Time stamps

--- app/Models/Buy.php
+++ app/Models/Buy.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Buy extends Model
 {
-    
+    public $timestamps = true;
 }
--- app/Models/History_price.php
+++ app/Models/History_price.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class History_price extends Model
 {
- 
+    public $timestamps = true;
 }
