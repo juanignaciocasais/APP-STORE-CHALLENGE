c8b340b831f6da96886dcc450069235f72923c92
juan <juanignaciocasais@gmail.com>
2021-01-31 01:36:25 -0300

    dropdown categories

--- resources/views/list-nav.blade.php
+++ resources/views/list-nav.blade.php
@@ -2,8 +2,26 @@
   <div class="form-group">
     <a href="{{route('my/apps')}}" class="list-group-item list-group-item-action active">My Apps</a>
   </div>
-  <li class="list-group-item list-group-item-action active">Categories</li>
-  @foreach($categories as $category)
-  <a href="{{route('filter', $category->category_id)}}" class="list-group-item list-group-item-action">{{$category->category_name}}</a>
-  @endforeach
+  <ul class='nav'>
+    <li>
+      <div class="dropdown">
+        <a class="dropdown-toggle list-group-item list-group-item-action active" data-toggle="dropdown" href="#">Categories</a>
+        <ul class="dropdown-menu" role="menu" aria-labelledby="dLabel">
+          @foreach($categories as $category)
+          <li><a href="{{route('filter', $category->category_id)}}">{{$category->category_name}}</a></li>
+          @endforeach
+          <li class="divider"></li>
+        </ul>
+      </div>
+    </li>
+  </ul>
 </div>
+
+@section('js')
+<script>
+  var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+  var dropdownList = dropdownElementList.map(function(dropdownToggleEl) {
+    return new bootstrap.Dropdown(dropdownToggleEl)
+  })
+</script>
+@stop
